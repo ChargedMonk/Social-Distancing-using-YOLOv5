6a28e02f89189f8e974fabe98a841dcf70f277ed
Vatsalya Bajpai <vatsalyabajpai03@gmail.com>
2020-06-21 01:01:01 +0530

    Removed the wrong classification bug

--- utils/utils.py
+++ utils/utils.py
@@ -919,15 +919,21 @@ def plot_one_box(x, img, color=None, label=None, line_thickness=None):
     #     cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
 
 def plot_dots_on_people(x, img):
-    # Plotting centers of people with yellow dot.
+    # Plotting centers of people with green dot.
     thickness = -1;
-    color = [0, 255, 255] # yellow
+    color = [0, 255, 0] # green
     center = ((int(x[2])+int(x[0]))//2,(int(x[3])+int(x[1]))//2)
     radius = 10
     cv2.circle(img, center, radius, color, thickness)
 
 def distancing(people_coords, img, dist_thres_lim=(200,250)):
     # Plot lines connecting people
+    already_red = dict() # dictionary to store if a plotted rectangle has already been labelled as high risk
+    centers = []
+    for i in people_coords:
+        centers.append(((int(i[2])+int(i[0]))//2,(int(i[3])+int(i[1]))//2))
+    for j in centers:
+        already_red[j] = 0
     x_combs = list(itertools.combinations(people_coords,2))
     radius = 10
     thickness = 5
@@ -941,22 +947,28 @@ def distancing(people_coords, img, dist_thres_lim=(200,250)):
             color = (0, 255, 255)
             label = "Low Risk "
             cv2.line(img, cntr1, cntr2, color, thickness)
-            cv2.circle(img, cntr1, radius, color, -1)
-            cv2.circle(img, cntr2, radius, color, -1)
+            if already_red[cntr1] == 0:
+                cv2.circle(img, cntr1, radius, color, -1)
+            if already_red[cntr2] == 0:
+                cv2.circle(img, cntr2, radius, color, -1)
             # Plots one bounding box on image img
             tl = round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
             for xy in x:
-                c1, c2 = (int(xy[0]), int(xy[1])), (int(xy[2]), int(xy[3]))
-                tf = max(tl - 1, 1)  # font thickness
-                t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
-                c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
-                cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
-                cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
-                cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+                cntr = ((int(xy[2])+int(xy[0]))//2,(int(xy[3])+int(xy[1]))//2)
+                if already_red[cntr] == 0:
+                    c1, c2 = (int(xy[0]), int(xy[1])), (int(xy[2]), int(xy[3]))
+                    tf = max(tl - 1, 1)  # font thickness
+                    t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+                    c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+                    cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+                    cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
+                    cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
 
         elif dist < dist_thres_lim[0]:
             color = (0, 0, 255)
             label = "High Risk"
+            already_red[cntr1] = 1
+            already_red[cntr2] = 1
             cv2.line(img, cntr1, cntr2, color, thickness)
             cv2.circle(img, cntr1, radius, color, -1)
             cv2.circle(img, cntr2, radius, color, -1)
